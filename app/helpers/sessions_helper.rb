bc2099acffd4dcc7e77f5df086db84e9d037caa3
tsubasa1122 <nakanot@open8.com>
2020-05-06 00:43:48 +0900

    [add]users index edit update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.[:remember_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
@@ -20,6 +20,10 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  def current_user?(user)
+    user && user == current_user
   end
 
   def logged_in?
@@ -36,5 +40,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
