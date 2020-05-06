7a039756206e7934f097a4ec3e3b535c9ca3056a
tsubasa1122 <nakanot@open8.com>
2020-05-07 00:22:25 +0900

    [add]Bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, self.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
